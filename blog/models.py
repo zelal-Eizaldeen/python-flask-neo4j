e958b2f286158e2232afc6ed00feedc73aeb9533
Zelal EizAldeen <zelalezaldeen@gmail.com>
2024-08-07 20:23:57 +0300

    Added publish relationships

--- blog/models.py
+++ blog/models.py
@@ -44,10 +44,6 @@ class User:
                 database_="users",
             )
             df = pd.DataFrame(records)
-
-            # df = df.rename(columns={0: 'Username'})
-            # user = df['Username']
-            print(df)
             return df
 
     def register(self, password):
@@ -75,18 +71,53 @@ class User:
         with GraphDatabase.driver(URI, auth=AUTH) as driver:
             driver.verify_connectivity()
         query = """CREATE CONSTRAINT  IF NOT EXISTS FOR (n:User) REQUIRE n.username IS UNIQUE"""
-        driver.execute_query(query)
+        driver.execute_query(query,database_="users")
         return True
         driver.close()
 
-    def create_user_constraint(self):
+    def add_post(self, title, tags, text):
+        user = self.find()
+        today = datetime.now()
+
         with GraphDatabase.driver(URI, auth=AUTH) as driver:
             driver.verify_connectivity()
-        query = """CREATE CONSTRAINT  IF NOT EXISTS FOR (n:User) REQUIRE n.username IS UNIQUE"""
-        driver.execute_query(query)
-        return True
+        query = """ MERGE (p:Post {id: $id, title:$title, text:$text, timestamp:$timestamp, date:$date}) RETURN p.id, p.title, p.text, p.timestamp, p.date"""
+        records, summary, keys=driver.execute_query(
+            query,
+            {"id": str(uuid.uuid4()), "title": title, "text": text, "timestamp":int(today.strftime("%s")),"date":today.strftime("%F")},
+            database_="users",
+        )
+        post=pd.DataFrame(records)
         driver.close()
 
+
+        print(f"now let's query merge USER{user.iloc[0][0]} and then POST{post.iloc[0][0]}")
+
+        with GraphDatabase.driver(URI, auth=AUTH) as driver:
+            driver.verify_connectivity()
+
+        query = """MATCH(u:User {username: $username}),
+                        (p:Post)
+                       MERGE (u)-[r:PUBLISH]->(p)"""
+        records, summary, keys=driver.execute_query(
+                     query, {"username":user.iloc[0][0]}, database_="users")
+
+        print(f"this is after merging records after rel {records}")
+
+
+
+
+
+        # today_node = calendar.date(today.year, today.month, today.day).day
+        # graph.create(Relationship(post, "ON", today_node))
+        #
+        # tags = [x.strip() for x in tags.lower().split(",")]
+        # tags = set(tags)
+        #
+        # for tag in tags:
+        #     t = graph.merge_one("Tag", "name", tag)
+        #     rel = Relationship(t, "TAGGED", post)
+        #     graph.create(rel)
 
 # user1 = User("Hiba")
 # user1.register("YES")
